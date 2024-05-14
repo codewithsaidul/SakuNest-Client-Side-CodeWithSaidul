460d2f78b55cafd6ea5692430e54189a9fabd392
codewithsaidul <codewithsaidul@gmail.com>
2024-05-15 00:31:13 +0600

    Booking Cancelled Done

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import useAuth from "../hooks/useAuth"
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 
 const MyBookings = () => {
@@ -15,6 +16,20 @@ const MyBookings = () => {
     const getMyBookings = async () => {
         const { data } = await axios(`${import.meta.env.VITE_API_URL}/bookings/${user?.email}`);
         setMyBookings(data)
+    }
+
+
+    const handleBookingCancel = async(id,status) => {
+        
+        try {
+            const { data } = await axios.patch(`${import.meta.env.VITE_API_URL}/bookRoom/${id}`, { status})
+            console.log(data)
+
+            // Refresh UI
+            getMyBookings()
+        } catch  {
+            toast.error("Something Went Wrog")
+        }
     }
 
     return (
@@ -89,7 +104,7 @@ const MyBookings = () => {
                                                         
                                                         <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                             <div className="flex items-center gap-x-6">
-                                                                <button disabled={booking.status === "Cancelled"} className="disabled:cursor-not-allowed text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                                                                <button onClick={() => handleBookingCancel(booking._id, "Cancelled")} disabled={booking.status === "Cancelled"} className="disabled:cursor-not-allowed text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
                                                                         <path d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                                     </svg>
