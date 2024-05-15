c65038311e265855fb6426eedf7653b0f8609f27
codewithsaidul <codewithsaidul@gmail.com>
2024-05-15 21:40:34 +0600

    Propomtion MOdal added on home page visitor

---
+++ src/components/Modal.jsx
@@ -0,0 +1,25 @@
+
+import BannerBG from '../../public/banerBG.jpg'
+import { IoCloseSharp } from 'react-icons/io5'
+
+const Modal = ({ setShowModal }) => {
+
+    return (
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center z-50">
+            <div className="card w-[45%] bg-base-100 shadow-xl">
+                <figure><img src={BannerBG} alt="Shoes" /></figure>
+                <div className="p-5">
+
+                    <p>Treat yourself to a weekend escape! Book a Friday and Saturday night stay and get <span className='text-primary text-2xl font-title'>10%</span> off your entire booking.</p>
+                    <div className="card-actions justify-end">
+                        <button onClick={() => setShowModal(false)} className='absolute top-3 right-3 text-white p-1 bg-primary rounded-full'>
+                            <IoCloseSharp size={30} />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,51 +6,66 @@ import Newsletter from "../components/Newsletter"
 import ReviewSlider from "../components/ReviewSlider"
 import FeaturedRooms from "../components/FeaturedRooms"
 import { Helmet } from "react-helmet"
+import { useEffect, useState } from "react"
+import Modal from "../components/Modal"
 
 
 
 const Home = () => {
 
 
+  const [showModal, setShowModal] = useState(false)
+
   const rooms = useLoaderData() || [];
-  
+
 
   const featuredRooms = rooms.filter(room => room.specialOffers !== '');
-  
+
+
+  useEffect(() => {
+    setShowModal(true)
+  }, [])
 
   return (
     <div>
-       <div className="application">
-                <Helmet>
-                    <meta charSet="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <meta name="title" content="Your Room Booking Website" />
-                    <meta name="description" content="Find the perfect accommodation for your next adventure! Explore a wide range of rooms, suites, and apartments with our easy-to-use booking platform." />
-                    <title>SakuNest || Home</title>
-                </Helmet>
-            </div>
-        <div>
-            <Banner/>
-            <div className="my-20 px-4 sm:px-8 md:px-12 lg:px-16 mx-auto">
-              <h2 className="text-5xl text-center text-primary font-title font-black">Featured Rooms</h2>
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="title" content="Your Room Booking Website" />
+          <meta name="description" content="Find the perfect accommodation for your next adventure! Explore a wide range of rooms, suites, and apartments with our easy-to-use booking platform." />
+          <title>SakuNest || Home</title>
+        </Helmet>
+      </div>
+
+    <div>
+      {
+        showModal && <Modal setShowModal={setShowModal} />
+      }
+    </div>
+
+      <div>
+        <Banner />
+        <div className="my-20 px-4 sm:px-8 md:px-12 lg:px-16 mx-auto">
+          <h2 className="text-5xl text-center text-primary font-title font-black">Featured Rooms</h2>
 
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-20">
-                {
-                  featuredRooms.map(room => <FeaturedRooms key={room._id} room={room}/>)
-                }
-              </div>
-            </div>
-            <Newsletter/>
-            <ReviewSlider/>
-        </div>
-
-        <div className="my-20">
-          <div>
-            <ContactForm/>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-20">
+            {
+              featuredRooms.map(room => <FeaturedRooms key={room._id} room={room} />)
+            }
           </div>
-          <MyMaps/>
         </div>
+        <Newsletter />
+        <ReviewSlider />
+      </div>
+
+      <div className="my-20">
+        <div>
+          <ContactForm />
+        </div>
+        <MyMaps />
+      </div>
     </div>
   )
 }
